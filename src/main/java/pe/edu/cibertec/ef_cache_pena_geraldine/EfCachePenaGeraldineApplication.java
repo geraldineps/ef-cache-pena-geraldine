870e77439877f44e2f4804575a7fdaae232cf895
graldineps <geriifujioka@gmail.com>
2024-10-23 05:13:37 -0500

    proyecto cache

--- src/main/java/pe/edu/cibertec/ef_cache_pena_geraldine/EfCachePenaGeraldineApplication.java
+++ src/main/java/pe/edu/cibertec/ef_cache_pena_geraldine/EfCachePenaGeraldineApplication.java
@@ -2,8 +2,10 @@ package pe.edu.cibertec.ef_cache_pena_geraldine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class EfCachePenaGeraldineApplication {
 
 	public static void main(String[] args) {
